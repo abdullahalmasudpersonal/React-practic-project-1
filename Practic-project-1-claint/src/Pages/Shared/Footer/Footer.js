975ff59dfb91966b1685a0107e6231f09bec7441
masud <abdullahalmasud01737906772@gmail.com>
2022-05-31 23:12:04 +0600

    another update

--- Practic-project-1-claint/src/Pages/Shared/Footer/Footer.js
+++ Practic-project-1-claint/src/Pages/Shared/Footer/Footer.js
@@ -2,24 +2,30 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+
     return (
         <div className='footer '>
             <div className='container footer-dev'>
-                    <div className='footer-dev2'>
-                        <p>footer</p>
-                    </div>
+                <div className='footer-dev2'>
+                    <p>footer</p>
+                </div>
 
-                    <div className='footer-dev2'>
-                        <p>footer</p>
-                    </div>
+                <div className='footer-dev2'>
+                    <p>footer</p>
+                </div>
 
-                    <div className='footer-dev2'>
-                        <p>footer</p>
-                    </div>
+                <div className='footer-dev2'>
+                    <p>footer</p>
+                </div>
 
-                    <div className='footer-dev2'>
-                        <p>footer</p>
-                    </div>
+                <div className='footer-dev2'>
+                    <p>footer</p>
+                </div>
+            </div>
+            <div className='container'>
+                 <p>  . Copyright&copy;{year}</p>
             </div>
         </div>
     );
