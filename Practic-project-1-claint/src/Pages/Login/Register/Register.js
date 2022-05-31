d014f8dc5e7406e6c6c9ab7b17834a50e4159d47
masud <abdullahalmasud01737906772@gmail.com>
2022-06-01 01:55:04 +0600

    create env.local

--- Practic-project-1-claint/src/Pages/Login/Register/Register.js
+++ Practic-project-1-claint/src/Pages/Login/Register/Register.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Register = () => {
+    const submit= {
+ 
+    }
     return (
         <div className='container'>
             <h5>Register</h5>
+            <div>
+                <from onSubmit={submit}>
+                    <input type='text' />
+                    <input type='password' />
+                    <input type='submit' value='submit' />
+                </from>
+            </div>
         </div>
     );
 };
